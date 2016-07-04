bd69d4577ea973e29352ebded860985a79118369
Ryan Mattson <ryansmattson@gmail.com>
2016-07-04 16:43:16 -0500

    Ready for turning in.  All modes complete except PRO MODE (generate random movie).

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ $.ajax({
     url: 'http://www.omdbapi.com/?t=the+sandlot&y=&plot=short&r=json',
   }).then(function(data){
     //print to the DOM
-    // $('.movie1').append('<h3>Movie 1</h3>');
     $('#movie1').append('<img class="poster" src="' + data.Poster + '" height="375px" width="260px"></img>');
     $('#movie1').append('<ul class="info"></ul>');
     $('#movie1 ul').append('<li class="title">Title: <span class="movie-data">' + data.Title + '</span></li>');
@@ -49,17 +48,24 @@ $.ajax({
 
 
 //slidToggle menu buttons
-$('main').on('click', '.label-info', function(){
-  $(this).closest('.menu').find('div').slideToggle();
+// $('main').on('click', '.label-info', function(){
+//   $(this).closest('.menu').find('div').slideToggle();
+// })
+
+$('.label').on('click', function(){
+  $(this).parent().find('div:first').slideToggle();
 })
 
 
-//Get results from serach.  Only works if you have exact title and year.  NEEDS TO BE FINISHED.
+//Get results from search.  Only works if you have exact title and year.  NEEDS TO BE FINISHED.
 $('form').on('submit', function(event){
   event.preventDefault();
+  $('#searched-movie').empty();
+  $('#searched-movie').css('display', 'inline');
   var tempTitle = $('#movie-title').val();
   var tempYear = $('#movie-year').val();
-  var tempURL = 'http://www.omdbapi.com/?t=' + tempTitle + '&y=' + tempYear + '&plot=short&r=json';
+  // var tempURL = 'http://www.omdbapi.com/?t=' + tempTitle + '&y=' + tempYear + '&plot=short&r=json';
+  var tempURL = 'http://www.omdbapi.com/?s=' + tempTitle;
 
   console.log(tempURL);
 
@@ -67,15 +73,17 @@ $('form').on('submit', function(event){
       url: '' + tempURL,
     }).then(function(data){
       //print to the DOM
-      $('#searched-movie').empty();
-      $('#searched-movie').css('display', 'inherit');
-      $('#searched-movie').append('<img class="poster" src="' + data.Poster + '" height="375px" width="260px"></img>');
-      $('#searched-movie').append('<ul class="info"></ul>');
-      $('#searched-movie ul').append('<li class="title">Title: <span class="movie-data">' + data.Title + '</span></li>');
-      $('#searched-movie ul').append('<li class="year">Year: <span class="movie-data">' + data.Year + '</span></li>');
-      $('#searched-movie ul').append('<li class="plot">Plot: <span class="movie-data">' + data.Plot + '</span></li>');
+      console.log(data.Search[0]);
+      for (i = 0; i < data.Search.length; i++){
+        console.log(data.Search[i]);
+        $('#searched-movie').append('<article class="newMovie" id="new-movie' + i + '"</article>');
+        $('#new-movie' + i).append('<img class="poster" src="' + data.Search[i].Poster + '" height="375px" width="260px"></img>');
+        $('#new-movie' + i).append('<ul class="info"></ul>');
+        $('#new-movie' + i).append('<li class="title">Title: <span class="movie-data">' + data.Search[i].Title + '</span></li>');
+        $('#new-movie' + i).append('<li class="year">Year: <span class="movie-data">' + data.Search[i].Year + '</span></li>');
+      }
     }).catch(function(){
-      //give error message
+      $('#searched-movie').append('No movies with that title could be found.');
   })
 
   console.log(tempTitle, tempYear);
